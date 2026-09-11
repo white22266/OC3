bf8655609c76807838cdd1cc893f54f5d457c60d
Jian Hao <peckjianhao0226@gmail.com>
2026-09-11 11:39:10 +0800

    refactor: remove corrupt inline company world data URI

--- apps/control-center/src/components/world/reference-art/index.ts
+++ apps/control-center/src/components/world/reference-art/index.ts
@@ -1,22 +1,4 @@
-import part00 from './part00';
-import part01 from './part01';
-import part02 from './part02';
-import part03 from './part03';
-import part04 from './part04';
-import part05 from './part05';
-import part06 from './part06';
-
 export const APPROVED_WORLD_ART_SIZE = { width: 1191, height: 768 } as const;
-
-export const approvedWorldArtDataUri = `data:image/webp;base64,${[
-  part00,
-  part01,
-  part02,
-  part03,
-  part04,
-  part05,
-  part06,
-].join('')}`;
 
 export const approvedWorldHotspots = {
   yoda: { x: 244, y: 365, width: 132, height: 146 },
